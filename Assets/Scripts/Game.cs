3785a9717385ba9dcf5da65169ca27c3868ba068
j-alizadeh <jalizade3d@yahoo.com>
2019-11-14 14:49:37 +0100

    The game

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+
+    //The minimum values are 2x2, so `OnValidate` makes sure of it
+    [SerializeField]
+    Vector2Int boardSize = new Vector2Int(11, 11);
+
+    [SerializeField]
+    GameBoard gameBoard = default;
+
+
+    private void OnValidate()
+    {
+        if(boardSize.x < 2)
+        {
+            boardSize.x = 2;
+        }
+
+        if (boardSize.y < 2)
+        {
+            boardSize.y = 2;
+        }
+    }
+
+    private void Awake()
+    {
+        gameBoard.Initialize(boardSize);
+    }
+}
