f0d7ad32350ed81a9dd5278c5cbd1cb51f4477f2
j-alizadeh <jalizade3d@yahoo.com>
2019-11-14 15:18:09 +0100

    Laying Tiles

--- Assets/Scripts/GameBoard.cs
+++ Assets/Scripts/GameBoard.cs
@@ -7,6 +7,9 @@ public class GameBoard : MonoBehaviour
     [SerializeField]
     Transform ground = default;
 
+    [SerializeField]
+    GameTile tilePrefab = default;
+    GameTile[] tiles;
 
     Vector2Int size;
 
@@ -14,5 +17,18 @@ public class GameBoard : MonoBehaviour
     {
         this.size = size;
         ground.localScale = new Vector3(size.x, size.y, 1f);
+        tiles = new GameTile[size.x * size.y];
+
+        for (int i = 0; i < size.x; i++)
+        {
+            for (int j = 0; j < size.y; j++)
+            {
+                GameTile tile = tiles[i] = Instantiate(tilePrefab,
+                    new Vector3(i - (size.x - 1) * 0.5f, 0f, j - (size.y - 1) * 0.5f),
+                    Quaternion.identity);
+                tile.name = "Tile [" + i + "," + j + "]";
+                tile.transform.SetParent(transform, false);
+            }
+        }
     }
 }
